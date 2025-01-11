6b8bbd305531773daa362eacdfad22e4168e3532
Bianca Di Biase <Bianca@Biancas-MacBook-Air.local>
2025-01-11 14:40:15 +0800

    changes made to settings.py

--- crowdfunding/crowdfunding/settings.py
+++ crowdfunding/crowdfunding/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'users.apps.UsersConfig',
     'rest_framework',
     'rest_framework.authtoken',
-    'rest_framework_simplejwt',
     'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
